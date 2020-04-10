4837f6675f11a26831071212289edd6d9266909c
hao.yanbing <binary_hao@163.com>
2020-04-10 20:14:20 +0800

    添加 SseEmitter

--- src/main/java/com/haoyanbing/sse/PushController.java
+++ src/main/java/com/haoyanbing/sse/PushController.java
@@ -1,12 +1,17 @@
 package com.haoyanbing.sse;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author haoyanbing
@@ -15,8 +20,15 @@ import java.time.LocalDateTime;
 @RestController
 public class PushController {
 
+    public static Map<String, SseEmitter> map = new ConcurrentHashMap<>();
+
+    /**
+     * 原生实现
+     * @param response
+     * @throws IOException
+     */
     @RequestMapping("/push")
-    public void getStreamDataImprove(HttpServletResponse response) throws IOException {
+    public void push(HttpServletResponse response) throws IOException {
         response.setContentType("text/event-stream");
         response.setCharacterEncoding("utf-8");
         String s = "retry:1000\n";
@@ -28,6 +40,37 @@ public class PushController {
                 pw.flush();
             } catch (InterruptedException e) {
                 e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Spring sseEmitter 实现
+     * @param id
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping(value = "/connect")
+    public SseEmitter connect(@RequestParam("id") String id) throws IOException {
+        SseEmitter sseEmitter = new SseEmitter(0L);
+        map.put(id, sseEmitter);
+        sseEmitter.send(SseEmitter.event().comment("comment"));
+        return sseEmitter;
+    }
+
+    /**
+     * 发送消息
+     * @param id
+     * @param message
+     * @throws IOException
+     */
+    @GetMapping("send")
+    public void send(@RequestParam("id") String id, @RequestParam("message") String message) throws IOException {
+        if (map.containsKey(id)) {
+            try {
+                map.get(id).send(SseEmitter.event().reconnectTime(2000).data(message));
+            } catch (Exception e) {
+                map.remove(id);
             }
         }
     }
